cd8b93c384775f8426e1f00a9b2269bc2fec19d9
wijdane <wa20010702@gmail.com>
2023-06-27 16:12:20 -0700

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
-
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     """print x elements of a list.
+
     Args:
         my_list (list): list of all elements.
         x (int):  number of time you need to go over the list
